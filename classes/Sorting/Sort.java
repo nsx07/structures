a88f9b82d31e56397c64e8c411fe882ebdc4f1bd
nsx07 <felipeaugustodluz@hotmail.com>
2023-10-26 15:21:20 -0300

    finalizando algortimos

--- classes/Sorting/Sort.java
+++ classes/Sorting/Sort.java
@@ -1,5 +1,8 @@
 package classes.Sorting;
 
+import java.time.Duration;
+import java.time.Instant;
+
 public class Sort {
     
     private static int[] Bubble(int[] values, int skip) {
@@ -21,35 +24,61 @@ public class Sort {
     }
 
     public static int[] Bubble(int[] values) {
-        return Bubble(values, 1);
+        Timer timer = new Timer();
+        timer.start();
+        int[] bubble = Bubble(values, 1);
+        System.out.println("Bubble: " + timer.elapsed());
+        return bubble;
     }
 
     public static int[] Insertion(int[] values) {
         int temp;
 
+        Timer timer = new Timer();
+        timer.start();
         for (int i = 1; i < values.length; i++) {
-            if (values[i] < values[i - 1]) {
-                temp = values[i];
-                values[i] = values[i - 1];
-                values[i - 1] = temp;
+            for (int j = i; j > 0; j--) {
+                if (values[j - 1] > values[j]) {
+                    temp = values[j];
+                    values[j] = values[j - 1];
+                    values[j - 1] = temp;
+                    break;
+                }
             }
         }
-        
-        
+
+        System.out.println("Insertion: " + timer.elapsed());
+
         return values;
+
+    }
+    private static class Timer {
+        private Instant start;
+
+        public void start() {
+            this.start = Instant.now();
+
+        }
+
+        public long elapsed() {
+            return Duration.between(start, Instant.now()).getNano();
+        }
     }
 
 
     public static void main(String[] args) {
         int[] a = new int[] {2,8,12,7,1};
-        // int[] b = Bubble(a);
-        int[] i = Insertion(a);
-        
-        // for (int bi : b) {
-        //     System.out.println(bi);
-        // }
+        int[] copy = new int[] {2,8,12,7,1};
+        int[] bubble = Bubble(a);
+        int[] insertion = Insertion(copy);
 
-        for (int j : i) {
+        System.out.println("\n Bubble Sort");
+        for (int bi : bubble) {
+             System.out.println(bi);
+        }
+
+        System.out.println("\n Insertion Sort");
+        for (int j : insertion) {
             System.out.println(j);
         }
     }
